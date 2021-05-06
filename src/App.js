f73bac3d5c889cfea8397521d0e8bf99e4fdd91a
Gareth Pile <gareth.pile@leansystems.co.za>
2021-05-06 11:55:38 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header>
-      <img src="./images/360dsl_logo.png" alt="360DSL" />
+      
         <h1>Please connect your accounts to your 360DSL account:</h1>
         <a href = "https://oauth.sandbox.trainingpeaks.com/OAuth/Authorize?client_id=m360&response_type=code&scope=workouts athlete:profile&redirect_uri=https://cisx9pt2th.execute-api.us-east-1.amazonaws.com/dev/tpnotification">Connect your TP account</a>
         <a href = "http://www.strava.com/oauth/authorize?client_id=7947&response_type=code&scope=activity:read_all&redirect_uri=https://cisx9pt2th.execute-api.us-east-1.amazonaws.com/dev/notification">Connect your Strava account</a>
