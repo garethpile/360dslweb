8c9e5b74bd9d4c9e55a5ac26262cb6caca65bfa4
Gareth Pile <gareth.pile@leansystems.co.za>
2021-12-12 05:31:50 +0200

    auth

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ function App() {
     bypassCache: false, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
   })
     .then(
-      (user) => (userId = user.username)
+      (user) => {
+        userId = user.username;
+        console.log(userId);
+      }
       //userId = user.username
     )
     .catch((err) => console.log(err));
