b2d880c20e0d769e8abf93df33d192bf501536f0
Gareth Pile <gareth.pile@leansystems.co.za>
2021-12-12 08:16:14 +0200

    auth

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
         </a>
       </div>
       <div>
-        <a href="https://7t2zui1c0h.execute-api.us-east-1.amazonaws.com/staging/requesttoken?userId=${userId}">
+        <a href='https://7t2zui1c0h.execute-api.us-east-1.amazonaws.com/staging/requesttoken?userId=${userId}'>
           Connect your Garmin account
         </a>
       </div>
@@ -60,7 +60,7 @@ export default withAuthenticator(App);
 function fetchGarminToken() {
   const headers = { "Content-Type": "application/json" };
   fetch(
-    "https://ab3qw9gu7b.execute-api.us-east-1.amazonaws.com/staging/requesttoken?userId=${userId}"
+    "https://ab3qw9gu7b.execute-api.us-east-1.amazonaws.com/staging/requesttoken"
   )
     .then((response) => response.json())
     .then((data) => {
