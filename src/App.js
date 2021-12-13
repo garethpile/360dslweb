bcaf872cc520d2a6d599a826ab391cff3cb40086
Gareth Pile <gareth.pile@leansystems.co.za>
2021-12-13 07:17:47 +0200

    auth

--- src/App.js
+++ src/App.js
@@ -1,76 +1,55 @@
 import React from "react";
-//import logo from "./images/360dsl_logo.svg";
 import "./App.css";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import { actionButton } from "@aws-amplify/ui";
 import { Auth } from "aws-amplify";
 
 function App() {
   var userId;
 
+  // Obtain current logged in Amplify user userId which needs to be passed into Garmin URL later
   Auth.currentAuthenticatedUser({
     bypassCache: false, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
   })
-    .then(
-      (user) => {
-        userId = user.username;
-        console.log("Current userId: ", userId);        
-      }
-      //userId = user.username
-    )
+    .then((user) => {
+      userId = user.username;
+      console.log("Current userId: ", userId); // This works and userId visible ...
+    })
     .catch((err) => console.log(err));
 
-    return (
-      <div className="App">
-        <div>
-          <img
-            src="http://www.360dsl.co.za/attachments/Logo/360SL_logo_1.png"
-            alt="360DSL"
-            width="400"
-            height="300"
-          />
-        </div>
-        <h1>Please connect your Training Accounts to your 360DSL account:</h1>
-        <div>
-          <a href="https://oauth.sandbox.trainingpeaks.com/OAuth/Authorize?client_id=m360&response_type=code&scope=workouts athlete:profile&redirect_uri=https://cisx9pt2th.execute-api.us-east-1.amazonaws.com/dev/tpnotification">
-            Connect your TP account
-          </a>
-        </div>
-        <div>
-          <a href="http://www.strava.com/oauth/authorize?client_id=7947&response_type=code&scope=activity:read_all&redirect_uri=https://6kjj2t9ega.execute-api.us-east-1.amazonaws.com/staging/oauthexchange">
-            Connect your Strava account
-          </a>
-        </div>
-  
-        <div>
-          <a
-            href={`https://7t2zui1c0h.execute-api.us-east-1.amazonaws.com/staging/requesttoken/?userId="${userId}`}
-          >
-            Connect your Garmin account
-          </a>
-        </div>
-  
-        <AmplifySignOut />
+  // userId variable is not visoble in this return code below ????????
+  return (
+    <div className="App">
+      <div>
+        <img
+          src="./images/360dsl_logo.png"
+          alt="360DSL"
+          width="400"
+          height="300"
+        />
       </div>
-    );
+      <h1>Please connect your Training Accounts to your 360DSL account:</h1>
+      <div>
+        <a href="https://oauth.sandbox.trainingpeaks.com/OAuth/Authorize?client_id=m360&response_type=code&scope=workouts athlete:profile&redirect_uri=https://cisx9pt2th.execute-api.us-east-1.amazonaws.com/dev/tpnotification">
+          Connect your TP account
+        </a>
+      </div>
+      <div>
+        <a href="http://www.strava.com/oauth/authorize?client_id=7947&response_type=code&scope=activity:read_all&redirect_uri=https://6kjj2t9ega.execute-api.us-east-1.amazonaws.com/staging/oauthexchange">
+          Connect your Strava account
+        </a>
+      </div>
 
+      <div>
+        <a
+          href={`https://7t2zui1c0h.execute-api.us-east-1.amazonaws.com/staging/requesttoken/?userId="${userId}`}
+        >
+          Connect your Garmin account
+        </a>
+      </div>
+
+      <AmplifySignOut />
+    </div>
+  );
 }
 
 export default withAuthenticator(App);
-/*
-function fetchGarminToken() {
-  var url = new URL(
-    "https://7t2zui1c0h.execute-api.us-east-1.amazonaws.com/staging/requesttoken/"
-  );
-
-  var params = { userId: userId };
-
-  url.search = new URLSearchParams(params).toString();
-
-  //var url = fetch(url);
-
-  //console.log("url: ",url);
-
-  window.location.href = fetch(url);
-}
-*/
